da4f0adda48c83b8ebd46e451bdb48bfa91a02a8
Brian Shehu <brian.shehu2@gmail.com>
2024-10-25 17:49:52 -0400

    implemented useState

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,24 +1,42 @@
 import "./styles.css"
+import { useState } from "react"
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  
+    setTodos(currentTodos => [
+      ...currentTodos, 
+      { id: crypto.randomUUID(), title: newItem, completed: false }
+    ]);
+  
+    setNewItem(""); // Clear the input field after adding the item
+  }
+  
+
   return (
   <>
-    <form className= "new-item-form">
+    <form onSubmit= {handleSubmit} className= "new-item-form">
       <div className= "form-row">
         <label>New Item</label>
-        <input className= "item-form-description" type= "text" id="item" />
+        <input className= "item-form-description" value={newItem} onChange={e => setNewItem(e.target.value)} type= "text" id="item" />
       </div>
       <button className="btn">Add</button>
     </form>
     <h1 className= "header">Todo List</h1>
     <ul className= "list">
-      <li>
+      {todos.map (todo => {
+        return <li>
         <label>
-          <input type="checkbox" />
-            Item 1
+          <input type="checkbox" checked= {todo.completed} />
+            {todo.title}
         </label>
         <button className="btn btn-danger">Delete</button>
       </li>
+      })}
     </ul>
   </>
   )
