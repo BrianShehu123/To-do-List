2aa955c604bff01279721a61dfd4704654ad1135
Brian Shehu <brian.shehu2@gmail.com>
2024-10-24 20:54:41 -0400

    add some buttons and a todo list

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -6,10 +6,20 @@ export default function App() {
     <form className= "new-item-form">
       <div className= "form-row">
         <label>New Item</label>
-        <input type= "text" id="item" />
+        <input className= "item-form-description" type= "text" id="item" />
       </div>
       <button className="btn">Add</button>
     </form>
+    <h1 className= "header">Todo List</h1>
+    <ul className= "list">
+      <li>
+        <label>
+          <input type="checkbox" />
+            Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </ul>
   </>
   )
 }
