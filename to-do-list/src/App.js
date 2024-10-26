eb8ba70b37a016b8e5497642516e3c0453dd0992
Brian Shehu <brian.shehu2@gmail.com>
2024-10-26 18:47:35 -0400

    worked on the todo logic

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   const [todos, setTodos] = useState([])
 
   function handleSubmit(e) {
-    e.preventDefault();
+    e.preventDefault(); 
   
     setTodos(currentTodos => [
       ...currentTodos, 
@@ -15,6 +15,25 @@ export default function App() {
   
     setNewItem(""); // Clear the input field after adding the item
   }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id != id)
+    })
+  }
+
   
 
   return (
@@ -28,14 +47,17 @@ export default function App() {
     </form>
     <h1 className= "header">Todo List</h1>
     <ul className= "list">
+      {todos.length === 0 && "No List"}
       {todos.map (todo => {
-        return <li>
-        <label>
-          <input type="checkbox" checked= {todo.completed} />
-            {todo.title}
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
+        return (
+          <li key={todo.id}>
+            <label>
+              <input type="checkbox" checked= {todo.completed} onChange= {e => toggleTodo(todo.id, e.target.checked)} />
+              {todo.title}
+            </label>
+           <button onClick={() => deleteTodo(todo.id)}className="btn btn-danger">Delete</button>
+          </li>
+          )
       })}
     </ul>
   </>
