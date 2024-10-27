155b837502d594a2498f0308e786e3d1833b6c6b
Brian Shehu <brian.shehu2@gmail.com>
2024-10-26 20:56:29 -0400

    implemented the usseEffect hook

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,25 +1,33 @@
 import "./styles.css"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { NewTodoForm } from "./NewTodoForm"
 import { TodoList } from "./TodoList"
 
 export default function App() {
-  
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(title) {
-      setTodos(currentTodos => [
-          ...currentTodos, 
-          { id: crypto.randomUUID(), title, completed: false }
-        ]);
+    setTodos(currentTodos => [
+      ...currentTodos,
+      { id: crypto.randomUUID(), title, completed: false }
+    ]);
   }
 
-  
+
 
   function toggleTodo(id, completed) {
     setTodos(currentTodos => {
       return currentTodos.map(todo => {
-        if(todo.id === id) {
+        if (todo.id === id) {
           return { ...todo, completed }
         }
 
@@ -34,13 +42,13 @@ export default function App() {
     })
   }
 
-  
+
 
   return (
-  <>
-    <NewTodoForm  onSubmit={addTodo}/>
-    <h1 className= "header">Todo List</h1>
-    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
-  </>
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
   )
 }
